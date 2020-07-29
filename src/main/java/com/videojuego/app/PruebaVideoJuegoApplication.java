952facd9d90e794aaa7406c011c896af4667a3c7
dark <dark@Dark.dark>
2020-07-28 22:24:30 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/videojuego/app/PruebaVideoJuegoApplication.java
@@ -0,0 +1,13 @@
+package com.videojuego.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaVideoJuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaVideoJuegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videojuego/app/PruebaVideoJuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videojuego.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaVideoJuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
